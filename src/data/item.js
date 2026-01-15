220280fd9a63c739e422521ea7df1cc86b622808
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-01-15 16:30:13 +0700

    add product grid and its content

---
+++ src/data/item.js
@@ -0,0 +1,56 @@
+export const items = [
+  {
+    name: "Kecap",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuB0pLxy2gpN06HyELTAV9hHihArUgFM6_LjY4NEJMbpALtpGHVYaLy8_aaV-WqbIg5jcORcovSzeGBm0kmbHm_rc3Eyh1WjjZJck95QC1Ed74A2Fp2-UEHeU-NUjANKOWDNx8zGmjAVEznz7BrgCV0JUGDeOWxAwZp9r1hOnmti-qswQ6p_ArTGJvenZi5xX5ybrhuQwKsFalnU_Rm-phOc27jUwJtePJ35UeS2hYp-SnjINi6tTi0U5Swzy13jaQZbeUECyhqrntVJ",
+  },
+  {
+    name: "Merica Bubuk",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuBd1z0fjTDtST1d9pj6nVYu_LtcQ-eZYiPJTt---TW5z5w_VXJ3X_S_xi7hzRDg7EvcRVni0Ef4KiGKL0juK-5fdRMyYq46bLTb2hq53TaGHCmlkjWvcD6mMVR4CT3nBnWUU5I_gIvGYGSOKcTq4gyxxQ8ak1kDAZMPB3iNY3kTEbsm9xqxA637OCc4HHt1rNyVSGYki8g609ZdkbeAHkGTiBEZSnSqZLcrvsnhTloAK5ZEQufmXD2uRIq5D_yfEiY8-GK0QWyzawK1",
+  },
+  {
+    name: "Garam",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuBhWWqHHROT9d8wMEccP5OwJHOKTlXc0OM1nVLzEECI7C_1a4u67LO3aGasT01jIWWszl4Bf_fWY8QF6hp-g69wJ7Qg1JIPxFgmrJd4cE04-Tf2Ar9Y_j-ZgEf6LMldPyF7WMkvVevr4y8P1kIhjbRUQ3gfPvuPh4EPV5ZYDRE3PlPR7tHYPo-l56cH_rdwCwws907zo0JYNJSJ31FqwTfJVi50Y4NR6LKNgoD2G6wwFeA93ueg87HaeoUsAukNPr4KX0EaSCBeUbz1",
+    imgAlt: "Garam",
+  },
+  {
+    name: "Minuman Botol Kemasan",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuBT6XFmDCCHkbr4TtlFMMGUlksa3egyeGCQEzEbYbpJNAeu8ZJk3bbIIrV43_G35YMC_DboxukR7ORle8geR6PWmuWC-MyK3afQX1XNuUIwzauuPJSRV89rLlymzcp0WFA0bilQhww1WT-hLCgkRwB3RigN6UCZCuW3hwpLsz8mOh5cjgSM6RuuBU8aelFlfQp7k0Lm3hljfhzUZaSUpJ9mav3ticjHY5ZpScIvYHGqvERv9rXHauEh1x46WD6WYXDVtuEkaNLXn2MV",
+    imgAlt: "Minuman Botol",
+  },
+  {
+    name: "Camilan Makaroni",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuD8xy79CJQ2E76UAKaDQca1EPGua5LE7c9bD_wgVSVzu1AqP8h-T2jeQK72YBwjm6wTyv0XuOxdGcykorYvVvi2wyXrCVVqKFli8nBCwaZUCM5coXpxEugNeZMcW33b2-ezWBFCOoi3p1cKNNrE2d6FwxblXT0-QKeVbY4frzeVV4OhYLSG-NIiTnOg5JOUevUCwnMTt2Jc39h3FAVHyHWMZ6pEPD-SBDjgcY7xnPvdWJcZbcCN6XWcYoJ5Q4G3z8vDgc3c-FfYxfvP",
+    imgAlt: "Makaroni",
+  },
+  {
+    name: "Minyak Goreng",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuAAMUkvN_Uytc1h1D1nstTVlZBlOyNib2ZdSoOcrPXcKGkRAKwf7oPgfIlyU94U51OV3G0O_DOyCTKtFpe69rz6vNWNo8a3l6DPJm37fwG6QI3w_1JYpy9jeZPbMb7WCePaBZDLbIIRVIqpqSxjmV5w0AVkvWiv1U3bdohRMSCR7cXuUYhMaoaJP2SWBG03i73SGj30X_k3QahJ1-_TycZ8hftH1-2oIuWJPWQ9yVCPj0NYRMrGzPfmMZoAXIz0JEXISIFNHrSeKNmK",
+    imgAlt: "Minyak Goreng",
+  },
+  {
+    name: "Kopi Bubuk",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuD_RIaPDwexvR1r7YtuZS3C4dnD6dFK7HSnJsyQSKMPRI0MZsuasoQJBajKuv_QCwbi9hU118U5D2itTK4D_fKgkrbOl6-xOhofj9bYLj9bLPQ-6HSpa9XASvTFM_miUQm6vhtNdqGuAObvTfruGKFIOlZEkQkDZdafI_Tzbz8O0sGerZkJMwsiaodgfrnrYg3w2ZIRp1QhZByfg6DThceZKr2WYRC2zCYuGTPNTBnG71Lgo5wxmyMIaD-Mrkg_TCRN1YhMof5NmIkM",
+    imgAlt: "Kopi Bubuk",
+  },
+  {
+    name: "Teh Celup",
+    tag: "bumbu",
+    imgSrc:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuDCtI6gy7IDCyWVZiH1dp7cbG1Z4wO_ICOUa9sekbUT1TwIWFEU1EymGJT0dtgSEt5KvuqZxE0iT8bTJSR7Q3wVRfWyJFYV4YCJT92bAiJaZHttFgLB_yoaU53D-WJpK05rVKIfVX-Hxvps0WGdeYscGw5SCWJUTv8zYeIRNexjRbjberHKqnH3d3FZ8w_mB3rOWzDkBx4z7d6b0lZqEoniXHI2Z3ouMGJ9SkZw78NS50FyFkXrKeQ33bwrOZSRKNlCs91zNCLOg1-5",
+    imgAlt: "Teh Celup",
+  },
+];
