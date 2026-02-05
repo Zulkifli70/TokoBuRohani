3fcba0430eddc9c01b5e9bf6cac21634ede07e65
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-02-05 11:05:35 +0700

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: node({ mode: "standalone" }),
   vite: {
     plugins: [tailwindcss()],
   },
